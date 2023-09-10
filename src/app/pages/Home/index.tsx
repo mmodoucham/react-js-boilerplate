15f76e07e6a59b4c0e4ae9798e1b300b72004b7e
Mo <mmodoucham@outlook.com>
2023-09-10 20:23:11 +0300

    Added UI Balls

--- src/app/pages/Home/index.tsx
+++ src/app/pages/Home/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { SuperBalls } from '@uiball/loaders'
 
 const Home: React.FC = () => {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <h1 className='text-4xl font-bold text-center'>Home</h1>
+    <div className='flex justify-center items-center h-screen flex-col'>
+      <div>
+        <SuperBalls size={100} speed={1} color='black' />
+      </div>
+      <div>
+        <h1 className='text-4xl font-bold text-center'>Home</h1>
+      </div>
     </div>
   )
 }
